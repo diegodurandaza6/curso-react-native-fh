754e58e09475a5cc7486c046814cdce024112e7f
Diego Duran <diegodurandaza6@gmail.com>
2021-05-28 08:34:42 -0500

    Creación de aplicación en react native y construcción de hola mundo

---
+++ 02-MiPrimeraApp/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+	return (
+		<View style={{
+			flex: 1,
+			// backgroundColor: 'red',
+			justifyContent: 'center'
+		}}>
+			<Text style={{
+				fontSize:45,
+				textAlign:'center'
+			}}>HOLA MUNDO</Text>
+		</View>
+	)
+}
+
+export default App;
