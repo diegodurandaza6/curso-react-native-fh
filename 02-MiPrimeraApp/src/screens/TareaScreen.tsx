bec4fb1e56b6c683bcede0e144a1c551dceb74b1
Diego Duran <diegodurandaza6@gmail.com>
2021-06-02 05:38:57 -0500

    Realización de tarea 2

--- 02-MiPrimeraApp/src/screens/TareaScreen.tsx
+++ 02-MiPrimeraApp/src/screens/TareaScreen.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
 		borderWidth: 10,
 		borderColor: 'white',
 		backgroundColor: '#5856D6',
-		alignSelf:'flex-end'
+		alignSelf:'flex-start'
 	},
 	cajaNaranja: {
 		width: 100,
@@ -38,12 +38,14 @@ const styles = StyleSheet.create({
 		alignSelf:'flex-start',
 	},
 	cajaAzul: {
-		width: 100,
+		// width: 100,
 		height: 100,
 		// flex: 1,
 		borderWidth: 10,
 		borderColor: 'white',
 		backgroundColor: '#28C4D9',
-		alignSelf:'center'
+		// alignSelf:'center'
+		left: 0,
+		right:0
 	},
 });
