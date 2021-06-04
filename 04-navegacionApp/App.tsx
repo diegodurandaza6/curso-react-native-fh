17b91602be58f4b3e95c43e953eb84e3b88bfd12
Diego Duran <diegodurandaza6@gmail.com>
2021-06-04 06:48:23 -0500

    Creación de proyecto para desarrollo de diferentes modalidades de navegacion desde dispositivos moviles, agregación de dependencias con el manejador de dependencias Yarn

---
+++ 04-navegacionApp/App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+		 <Text>App Screen</Text>
+	 </NavigationContainer>
+  );
+}
+
+export default App;
