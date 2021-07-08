c65edf0f1199c4faee979e08597fde61334adf45
Diego Duran <diegodurandaza6@gmail.com>
2021-07-08 05:27:42 -0500

    Configuración inicial del proyecto de componentes react native y funcionalidad de flatlist

---
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FlatList, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface MenuItem {
+	name: string;
+	icon: string;
+	components: string;
+}
+
+const menuItems = [
+	{
+		name: 'Animation 101',
+		icon: 'cube-outline',
+		components: 'Animation101Screen'
+	}
+]
+
+export const HomeScreen = () => {
+
+	const renderMenuItem = (menuItem: MenuItem) => {
+		return(
+			<View>
+				<Text>{menuItem.name} - {menuItem.icon}</Text>
+			</View>
+		)
+	}
+
+	return (
+		<View style={{flex:1}}>
+			<FlatList 
+				data={menuItems}
+				renderItem={
+					({item}) => renderMenuItem(item)
+				}
+				keyExtractor={(item) => item.name}
+			/>
+		</View>
+	)
+}
