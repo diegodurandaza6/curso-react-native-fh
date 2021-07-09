66cc8d1e476a092614576811f0eb76f589d599e4
Diego Duran <diegodurandaza6@gmail.com>
2021-07-09 06:21:06 -0500

    Uso de animaciones para efectos visuales implementanto Animated.view y custom hooks

---
+++ 06-RNComponents/src/screens/Animation102Screen.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
+import { useDraggableView } from '../hooks/useDraggableView';
+
+export const Animation102Screen = () => {
+
+	const {panResponder, pan} = useDraggableView()
+
+	return (
+		<View style={{...styles.container}}>
+			<Animated.View 
+				{...panResponder.panHandlers}
+				style={[pan.getLayout(), styles.purpleBox]} 
+			/>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	purpleBox: {
+		backgroundColor: '#75CEDB',
+		width: 150,
+		height: 150,
+		borderRadius: 4,
+	}
+});
--- 06-RNComponents/src/screens/HomeScreen.tsx
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -1,39 +1,59 @@
 import React from 'react'
 import { FlatList, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { FlatListMenuItem } from '../components/FlatListMenuItem';
+import { MenuItem } from '../interfaces/appInterfaces';
+import { styles } from '../theme/appTheme';
 
-interface MenuItem {
-	name: string;
-	icon: string;
-	components: string;
-}
-
-const menuItems = [
+const menuItems: MenuItem[] = [
 	{
 		name: 'Animation 101',
 		icon: 'cube-outline',
-		components: 'Animation101Screen'
+		component: 'Animation101Screen'
+	},
+	{
+		name: 'Animation 102',
+		icon: 'albums-outline',
+		component: 'Animation102Screen'
 	}
 ]
 
 export const HomeScreen = () => {
 
-	const renderMenuItem = (menuItem: MenuItem) => {
+	const {top} = useSafeAreaInsets();
+	
+	const renderListHeader = () => {
 		return(
-			<View>
-				<Text>{menuItem.name} - {menuItem.icon}</Text>
+			<View style={{marginTop:top + 20, marginBottom: 20}}>
+				<Text style={styles.title}>Opciones de menu</Text>
+			</View>
+		)
+	}
+
+	const itemSeparator = () => {
+		return(
+			<View style={{
+				borderBottomWidth: 1, 
+				opacity: 0.4, 
+				marginVertical: 8
+			}}>
+
 			</View>
 		)
 	}
 
 	return (
-		<View style={{flex:1}}>
+		<View style={{flex:1, ...styles.globalMargin}}>
+			
 			<FlatList 
 				data={menuItems}
 				renderItem={
-					({item}) => renderMenuItem(item)
+					({item}) => <FlatListMenuItem menuItem={item}/>
 				}
 				keyExtractor={(item) => item.name}
+				ListHeaderComponent={ renderListHeader }
+				ItemSeparatorComponent={ itemSeparator }
 			/>
 		</View>
 	)
